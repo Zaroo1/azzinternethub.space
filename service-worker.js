67534fe854d8fe938997f1db2ed851e79c70d811
Azz Internet Hub <abdulazizzaroo@gmail.com>
2026-03-21 23:37:23 +0000

    Add files via upload
    Signed-off-by: Azz Internet Hub <abdulazizzaroo@gmail.com>

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "azz-hub-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch (offline support)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
